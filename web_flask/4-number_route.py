da653e62bebb5d8b0953e69677ddc948a6d48021
Nmcleon <nmcleon10@gmail.com>
2023-12-27 02:46:47 -0800

    fix pep8 err

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -26,6 +26,7 @@ def c_text(text=None):
     replace underscore _ symbols with a space """
     return "C {}".format(text.replace('_', ' '))
 
+
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<string:text>', strict_slashes=False)
 def python_text(text='is_cool'):
