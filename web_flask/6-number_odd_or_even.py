a1e830de8e1cadfdc47f3ea9ba96638f14cec726
Nmcleon <nmcleon10@gmail.com>
2023-12-27 23:25:23 -0800

    fix pep8

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -48,6 +48,7 @@ def number_template(n=None):
     H1 tag: “Number: n” inside the tag BODY"""
     return render_template('5-number.html', n=n)
 
+
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
     """display a HTML page only if n is an integer:
