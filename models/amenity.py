a84b39099f5cfafed08bed24417f598245c1d4a6
N <51336368+Nmcleon@users.noreply.github.com>
2023-12-30 10:09:35 +0200

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -3,33 +3,9 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Table, ForeignKey
 from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
 class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-
-    __table_args__ = {'extend_existing': True}
-
-""" Define the association table for the many-to-many
-relationship between Place and Amenity"""
-
-place_amenity = Table(
-    'place_amenity',
-    Base.metadata,
-    Column(
-        'place_id',
-        String(60),
-        ForeignKey('places.id'),
-        primary_key=True,
-        nullable=False),
-    Column(
-        'amenity_id',
-        String(60),
-        ForeignKey('amenities.id'),
-        primary_key=True,
-        nullable=False),
-    extend_existing=True
-    )
-
-place_amenity_rel = relationship('Place', secondary=place_amenity,
-             viewonly=False)
+    place_amenity_rel = relationship('Place', secondary=place_amenity, viewonly=False)
