7939d15581ae9deee4ce86688f7a949d3a5cd7e0
N <51336368+Nmcleon@users.noreply.github.com>
2023-12-30 13:11:43 +0200

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,38 +1,28 @@
 #!/usr/bin/python3
-"""This is the state class"""
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
+""" State Module for HBNB project """
+from models.base_model import BaseModel
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String
-import models
-from models.city import City
-import shlex
+import os
 
 
-class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
+class State(BaseModel):
+    """ State class
+    name = ""
     """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                          backref="state")
 
-    @property
-    def cities(self):
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if (city[0] == 'City'):
-                lista.append(var[key])
-        for elem in lista:
-            if (elem.state_id == self.id):
-                result.append(elem)
-        return (result)
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship('City', cascade='all, delete', backref='state')
+    else:
+        @property
+        def cities(self):
+            """public getter method cities to
+            return the list of City"""
+            from models.city import City
+            from models import storage
 
             my_list = []
             all_cities = storage.all(City)
